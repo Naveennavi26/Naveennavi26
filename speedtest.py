991de71ba4e67ad06279ae918f8b967176cfb3f6
Naveennavi26 <47524323+Naveennavi26@users.noreply.github.com>
2021-07-22 12:26:43 +0530

    Create speedtest.py

---
+++ speedtest.py
@@ -0,0 +1,5 @@
+import pyspeedtest
+st = pyspeedtest.SpeedTest()
+st.ping()
+#print("your Connection's download speed is:", st.download())
+#print("your connection's Upload speed is :", st.upload())
