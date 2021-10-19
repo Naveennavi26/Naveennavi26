18ab7629d0aea18720cbd365226e70f0f31a71c8
Naveennavi26 <naveenvel26@gmail.com>
2021-10-19 11:18:36 +0530

    commit

--- hello.py
+++
@@ -1,1 +0,0 @@
-# This program prints Hello, world!
--- speedtest.py
+++
@@ -1,5 +0,0 @@
-import pyspeedtest
-st = pyspeedtest.SpeedTest()
-st.ping()
-#print("your Connection's download speed is:", st.download())
-#print("your connection's Upload speed is :", st.upload())
